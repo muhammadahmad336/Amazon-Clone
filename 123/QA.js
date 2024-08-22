c973f8e17c4ed8d903e7f4653388a756d214338d
Muhammad Ahmad <ahmadyaseenahmad336@gmail.com>
2024-08-22 07:36:26 -0700

    inital commit

---
+++ 123/QA.js
@@ -0,0 +1,23 @@
+// function f = (0,1,2,3,4)
+
+let Physics = 75
+let chemistry = 80
+let biology = 95
+let mathematics = 33
+let computer = 100
+// let (biology == 95);
+if(biology >= 90){
+    console.log("Grade A")
+}
+else if (Physics>=80){
+    console.log("Grade B")
+}
+else if (computer>=70){
+    console.log("Grade C")
+}
+else if (chemistry>=60){
+    console.log("Grade D")
+}
+else if (mathematics>=30){
+    console.log("Grade F")
+}
